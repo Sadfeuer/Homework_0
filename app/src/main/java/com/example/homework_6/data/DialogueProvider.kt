0917c318439448eed797ae62b61aece3121c4eb9
Sadfeuer <90156878+Sadfeuer@users.noreply.github.com>
2021-12-25 13:49:40 +0300

    Homework_7_8

--- app/src/main/java/com/example/homework_6/data/DialogueProvider.kt
+++
@@ -1,8 +0,0 @@
-package com.example.homework_6.data
-
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
-
-class DialogueProvider {
-    val alert1: String = "текущее значение: "
-    val alert2: String = "Сумма: "
-}
--- app/src/main/java/com/example/homework_6/data/HistoryItem.kt
+++
@@ -1,3 +0,0 @@
-package com.example.homework_6.data
-
-data class HistoryItem (val historyItem: Int)
